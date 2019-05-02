877662025393104824c14e8e431e28aae8e6b880
igraczech <suculent@me.com>
2019-05-02 10:11:59 +0200

    initial public commit

---
+++ app.py
@@ -0,0 +1,37 @@
+import time
+from flask import Flask
+from flask import send_file
+import redis
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return "Hit count: {}.\n".format(count)
+
+@app.route('/images/transparent.png')
+def image():
+    count = get_hit_count()
+    print("Hit count: {}.\n".format(count))
+    try:
+        return send_file('./transparent.png', attachment_filename='transparent.png')
+    except Exception as flask_exception:
+        print(str(flask_exception))
+        return ""
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
